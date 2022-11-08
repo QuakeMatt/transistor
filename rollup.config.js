8ba9512b288821ea69a886cb2056b5d468ffa779
Matthew Lawrence-Stubley <quakematt@outlook.com>
2022-11-08 18:42:16 +0000

    Create TypeScript environment

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            exports: 'default',
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+        },
+        {
+            file: './dist/transistor.js',
+            format: 'iife',
+            name: 'transistor',
+        },
+    ],
+    external: [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+        typescript({
+            typescript: require('typescript'),
+        }),
+    ],
+}
